96637e6fe8231b593c7caf8da53439d36da49787
Kerrick Staley <kerrick@kerrickstaley.com>
2019-05-14 05:33:11 -0700

    Implement and use nestCrates function
    nestCrates() takes the flat list of crates and builds a nested structure
    out of them (and also renames them appropriately).
    
    For example, a crate called ParentCrate%%ChildCrate is renamed to
    ChildCrate and nested under the crate called ParentCrate.

--- src/seratocrates.cpp
+++ src/seratocrates.cpp
@@ -39,6 +39,70 @@ private:
 };
 
 
+// Helper used in nestCrates
+std::vector<std::string> crateNamePieces(const Crate& crate) {
+  std::vector<std::string> ret;
+  size_t piece_start = 0;
+  for (size_t i = 1; i < crate.name.size(); i++) {
+    if (crate.name[i] == '%' && crate.name[i + 1] == '%') {
+      ret.push_back(crate.name.substr(piece_start, i - piece_start));
+      piece_start = i = i + 2;
+    }
+  }
+  ret.push_back(crate.name.substr(piece_start));
+  return ret;
+}
+
+
+// Take flat list of crates and create nested crate structure.
+// In this function's inputs, subcrates are named like "GrandparentCrate%%ParentCrate%%Crate".
+// In the output, there will be a crate called "GrandparentCrate" with a child called "ParentCrate"
+// which itself has a child called "Crate".
+// Destroys input.
+std::vector<Crate> nestCrates(std::vector<Crate>&& crates) {
+  // TODO This function copies a lot of data around; try to make it more efficient.
+  std::map<std::vector<std::string>, Crate*> pieces_to_crate;
+
+  for (auto& crate : crates) {
+    pieces_to_crate[crateNamePieces(crate)] = &crate;
+  }
+
+  for (auto it = pieces_to_crate.rbegin(); it != pieces_to_crate.rend(); it++) {
+    const std::vector<std::string>& pieces = it->first;
+    Crate* crate = it->second;
+
+    if (pieces.size() <= 1) {
+      continue;
+    }
+
+    std::vector<std::string> parent_pieces(pieces.begin(), pieces.end() - 1);
+
+    auto parent_it = pieces_to_crate.find(parent_pieces);
+    if (parent_it == pieces_to_crate.end()) {
+      // Crate's parent was not present. Silently ignore crate.
+      continue;
+    }
+
+    crate->name = pieces.back();
+    parent_it->second->subcrates.emplace_back(std::move(*crate));
+  }
+
+  std::vector<Crate> ret;
+  for (auto& pair : pieces_to_crate) {
+    const std::vector<std::string>& pieces = pair.first;
+    Crate* crate = pair.second;
+
+    if (pieces.size() > 1) {
+      continue;
+    }
+
+    ret.emplace_back(std::move(*crate));
+  }
+
+  return ret;
+}
+
+
 std::unique_ptr<Library> readLibrary(const std::string& path) {
   std::filesystem::path root_path{path};
   std::filesystem::path serato_dir_path = root_path / "_Serato_";
@@ -60,6 +124,8 @@ std::unique_ptr<Library> readLibrary(const std::string& path) {
 
     ret->crates.push_back(crate_reader.read(crate_path.native()));
   }
+
+  ret->crates = nestCrates(std::move(ret->crates));
 
   return ret;
 }
